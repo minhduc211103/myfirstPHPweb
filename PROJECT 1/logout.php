fdd4b8ea741fbba9ed4844a7164c143c1eda8c12
MINHDUC_INTERN_PHP <147626554+minhduc211103@users.noreply.github.com>
2025-04-24 13:22:06 +0700

    Update logout.php

--- PROJECT 1/logout.php
+++ PROJECT 1/logout.php
@@ -3,26 +3,26 @@
 
 session_start();
 
-// Xóa mọi dữ liệu session
+// xóa hết mọi dữ liệu session
 $_SESSION = [];
 
-// Xóa cookie session
+// xóa cookie session
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(
         session_name(),
         '',
         time() - 42000,
-        $params["path"],
+        $params["path"],           // lấy thông tin từ session hiện tại
         $params["domain"],  
         $params["secure"],
         $params["httponly"]
     );
 }
 
-// Hủy session
+// hủy hẳn session
 session_destroy();
 
-// Chuyển hướng và DừNG script ngay lập tức
+// Chuyển hướng và exit ngay lập tức
 header("Location: login.php");
 exit();
