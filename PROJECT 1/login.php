d87e2b791d5c54eaf36e9e07581b460ecae9c73b
MINHDUC_INTERN_PHP <147626554+minhduc211103@users.noreply.github.com>
2025-04-24 13:45:16 +0700

    Update login.php

--- PROJECT 1/login.php
+++ PROJECT 1/login.php
@@ -4,7 +4,7 @@ include "constant.php";
 
 echo '<link rel="stylesheet" href="login.css">';
 require_once 'connectDB.php';
-if (isset($_SESSION["name"]) || $_SESSION["name"] == "user") {
+if (isset($_SESSION["name"]) && $_SESSION["name"] == "user") {
     header("Location: showDuLieu.php");
     exit();
 }
